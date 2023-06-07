20ec97835efb19063d6995ae34c97ecaa13e1494
miso1210 <boringsogood@gmail.com>
2023-06-07 23:03:15 +0700

    update

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./containers/HomePage";
+import ActionPage from "./containers/ActionPage";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/pray",
+    element: <ActionPage />,
   },
 ]);
